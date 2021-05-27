b9c14afb339b449bb0f2fcf6bd2b60d79d545de0
ahron <ahron901@gmail.com>
2021-05-27 23:34:38 +0300

    Welcome screen

--- app/src/main/java/com/example/acatch/MainActivity.java
+++ app/src/main/java/com/example/acatch/MainActivity.java
@@ -2,13 +2,12 @@ package com.example.acatch;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.Activity;
 import android.os.Bundle;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
 
     FirebaseDatabase database = FirebaseDatabase.getInstance();
     DatabaseReference myRef = database.getReference("message");
@@ -19,5 +18,6 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         myRef.setValue("hi");
+        getSupportActionBar().hide();
     }
 }
